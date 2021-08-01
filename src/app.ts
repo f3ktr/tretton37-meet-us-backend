d9fd30473e2168e0b6f6e698aafd1bebb5293f50
f3ktr <46202047+f3ktr@users.noreply.github.com>
2021-08-01 23:56:29 +0200

    Meet Us page backend created

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from "express";
+import { getEmployees } from "./service/scrapper";
+import cors from "cors";
+const app: Application = express();
+
+const PORT = parseInt(process.env.PORT!) | 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(`app is running on ${PORT}`);
+});
+
+app.get("/employees", async (req: Request, res: Response) => {
+  let name: any = req.query.name;
+  let sort: any = req.query.sort;
+  let office: any = req.query.office;
+  await getEmployees(name, sort, office).then((data) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(data);
+  });
+});
+app.listen(PORT);
